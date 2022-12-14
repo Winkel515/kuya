610d6fc8887de8b449091a9f9f23a18fdcb37b8c
Winkel Yin <winkel.yin@MTLMAC006.local>
2022-12-13 19:46:24 -0500

    review section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material';
 import NavBar from '../components/NavBar';
 import { Comfortaa, Ledger } from '@next/font/google';
+import Image from 'next/image';
+import Review from '../components/Review';
 
 const comfortaa = Comfortaa({ subsets: ['latin'] });
 const ledger = Ledger({
@@ -21,10 +22,41 @@ export default function Home() {
         <p className={`${ledger.className}`}>Coffee Shop in Brossard</p>
         <p className={`${ledger.className}`}>Opening at 8:00 AM tomorrow</p>
         <button
-          className={`${ledger.className} bg-primary hover:bg-hover text-white mt-5 h-12 w-44 px-6 py-2 px-4 rounded`}
+          className={`${ledger.className} bg-primary hover:bg-hover text-white mt-5 h-12 w-52 w-52 h-14 px-4 rounded`}
         >
           CONTACT US
         </button>
+        <Image
+          className="my-20"
+          src={'/mainImg.jpeg'}
+          width={1000}
+          height={500}
+          alt="coffee"
+        />
+        <div className="flex flex-col items-center mx-20">
+          <hr className="bg-black my-5 w-14 h-0.5 bg-black rounded border-0" />
+          <h2 className={`${comfortaa.className} mb-10`}>TESTIMONIALS</h2>
+          <div className="flex">
+            <Review
+              author="Christopher C"
+              text="Phil and the crew are really amazing at what they do. This place
+        is a must. Prices are more than fair considering how unique of a product
+        and experience they are delivering here daily. I mean, the strip mall
+        was empty, but Cafe Kuya was packed! What does that tell you about this
+        place? This place is great for students and families. I recommend the
+        ube iced latte as a drink and for food; everything. Amazing atmosphere,
+        love this place! God bless. John 3:16"
+            />
+            <Review
+              author="Tara S"
+              text="Not only do they serve amazing coffee and food, the staff couldn't be nicer. They always make you feel welcomed and take the time to ask how your day is going. So glad to see a sweet locally owned cafe in Brossard!"
+            />
+            <Review
+              author="Charles N"
+              text="Wonderful local café where family, friends and newcomers can come refuel on their favourite coffee brew, grab a bite with Filipino inspired sandwiches and wonderful dessert pies!"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      primary: '#48487f',
-      hover: '#3d3d6e',
-      white: '#fff',
+    extend: {
+      colors: {
+        primary: '#48487f',
+        hover: '#3d3d6e',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
