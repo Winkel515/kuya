0f95d64eb40a74ce6e5c6088ac42e310d330059c
Winkel Yin <winkel.yin@MTLMAC006.local>
2022-12-13 20:02:49 -0500

    added margin for review

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         <div className="flex flex-col items-center mx-20">
           <hr className="bg-black my-5 w-14 h-0.5 bg-black rounded border-0" />
           <h2 className={`${comfortaa.className} mb-10`}>TESTIMONIALS</h2>
-          <div className="flex">
+          <div className="flex flex-wrap">
             <Review
               author="Christopher C"
               text="Phil and the crew are really amazing at what they do. This place
